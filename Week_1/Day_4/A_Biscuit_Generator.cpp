ee8e76ad8dccbef63620f9168e7f786119da36b7
ItIsSagor <140883120+ItIsSagor@users.noreply.github.com>
2024-03-18 21:04:31 +0600

    Add files via upload
    Added codes of Week 1 Day 4

---
+++ Week_1/Day_4/A_Biscuit_Generator.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author:  SAGOR AHMED
+ *    created: M A R C H , 2024       
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long A, B, T;
+    cin >> A >> B >> T;
+    cout << (T/A)*B << '\n';
+    return 0;
+}
