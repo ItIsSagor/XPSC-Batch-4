7bd802ec0591065ca52c7fe52305b29c6ec0d20b
ItIsSagor <140883120+ItIsSagor@users.noreply.github.com>
2024-03-23 21:11:54 +0600

    Add files via upload
    Added Codes of Week 2 Day 2

---
+++ Week_2/Day_2/B_Remove_It.cpp
@@ -0,0 +1,24 @@
+/**
+ *    author:  SAGOR AHMED
+ *    created: M A R C H , 2024       
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (v[i] != x) {
+            cout << v[i] << " ";
+        }
+    }
+    return 0;
+}
