85962f599fb3d6d22a02b468b60e449e707b1146
ItIsSagor <140883120+ItIsSagor@users.noreply.github.com>
2024-09-05 17:30:13 +0600

    Add files via upload
    Added Codes of Week 19 Day 4

---
+++ Week_19/Day_4/A_Addition_and_Minimum.cpp
@@ -0,0 +1,89 @@
+/**
+ *    author:  SAGOR AHMED
+ *    created: 2 0 2 4       
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxN = 1e5 + 9, inf = 1e18;
+long long arr[maxN], v[4 * maxN], lazy[4 * maxN];
+
+void push(int n, int b, int e) {
+    if (lazy[n] == 0) {
+        return;
+    }
+    v[n] += lazy[n];
+    if (b != e) {
+        int l = (2 * n), r = (2 * n) + 1;
+        lazy[l] += lazy[n];
+        lazy[r] += lazy[n];
+    }
+    lazy[n] = 0;
+}
+
+void build(int n, int b, int e) {
+    if (b == e) {
+        v[n] = arr[b];
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    v[n] = min(v[l], v[r]);
+}
+
+void update(int n, int b, int e, int i, int j, int val) {
+    push(n, b, e);
+    if (e < i || j < b) {
+        return;
+    }
+    if (b >= i && e <= j) {
+        lazy[n] = val;
+        push(n, b, e);
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    update(l, b, mid, i, j, val);
+    update(r, mid + 1, e, i, j, val);
+    v[n] = min(v[l], v[r]);
+}
+
+long long query(int n, int b, int e, int i, int j) {
+    push(n, b, e);
+    if (e < i || b > j) {
+        return inf;
+    }
+    if (b >= i && e <= j) {
+        return v[n];
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    return min(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    // for (long long i = 1; i <= n; i++) {
+    //     cin >> arr[i];
+    // }
+    build(1, 1, n);
+    while (m--) {
+        int x;
+        cin >> x;
+        if (x == 1) {
+            int l, r, val;
+            cin >> l >> r >> val;
+            l++;
+            update(1, 1, n, l, r, val);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            l++;
+            cout << query(1, 1, n, l, r) << '\n';
+        }
+    }
+    return 0;
+}
